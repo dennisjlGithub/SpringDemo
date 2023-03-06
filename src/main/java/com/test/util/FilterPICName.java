f68a423135c15252215d68d7adba97d7d2d0fde5
denni <denni@DD>
2023-03-06 20:21:13 +0800

    dd

--- src/main/java/com/test/util/FilterPICName.java
+++ src/main/java/com/test/util/FilterPICName.java
@@ -1,55 +1,40 @@
 package com.test.util;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FilterPICName {
-	private static final String rootPath = "D:\\Temp\\Done\\a";
-	private static final String suffix = "*.{jpg,jar,txt,pdf}";
-	
-	public static void main(String[] args) {
-		
-		RenameFiles.listFileNames(rootPath);
-//		RenameFiles.rename1(rootPath);
-		
+	private static final String rootPath = "D:\\Temp\\Done\\";
+	private static final String suffix = "*.{jpg,ico}";
+
+	public static void main(String[] args) throws IOException {
+		FilterPICName.removeFiles();
 	}
-	
-	public static void listFileNames(String filePath) {
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(Path.of(filePath), suffix)) {
-		    for (Path file: stream) {
-		        System.out.println(file.getParent() + "\\"+ file.getFileName());
-		    }
-		} catch (IOException | DirectoryIteratorException x) {
-		    System.err.println(x);
+
+	public static void removeFiles() throws IOException {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(rootPath))) {
+			for (Path path : stream) {
+				if (Files.isDirectory(path)) {
+					String dir = path.getFileName().toString();
+					System.out.println("Process folder: " + dir);
+					FilterPICName.removeFilesbyFolder(rootPath + dir);
+				}
+			}
 		}
 	}
-	
-	public static void rename(String filePath, String newName) {
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(Path.of(filePath), suffix)) {
-		    for (Path file: stream) {
-		        File oldFile = new File(file.getParent() + "\\" + file.getFileName());
-		        File newFile = new File(file.getParent() + "\\" + newName + file.getFileName());
-		        oldFile.renameTo(newFile);
-		    }
-		} catch (IOException | DirectoryIteratorException x) {
-		    System.err.println(x);
+
+	public static void removeFilesbyFolder(String dir) throws IOException {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(Path.of(dir), suffix)) {
+			for (Path file : stream) {
+				String fileName = file.getParent() + "\\" + file.getFileName();
+				if (fileName.endsWith("ico") || fileName.contains("\\0")) {
+					Files.deleteIfExists(Paths.get(fileName));
+				}
+			}
 		}
 	}
-	
-	public static void rename1(String filePath) {
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(Path.of(filePath), suffix)) {
-		    for (Path file: stream) {
-		        File oldFile = new File(file.getParent() + "\\" + file.getFileName());
-		        File newFile = new File(file.getParent() + "\\" + "2020_" + file.getFileName().toString().substring(10, 12) + ".pdf");
-		        oldFile.renameTo(newFile);
-		    }
-		} catch (IOException | DirectoryIteratorException x) {
-		    System.err.println(x);
-		}
-	}
-		
+
 }
